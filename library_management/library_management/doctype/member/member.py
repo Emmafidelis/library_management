d72fea49f35f3c5f6af392424fcdc8699a6f4896
Emanuel Fidelis <emanuelkagombora28@gmail.com>
2024-10-25 15:26:37 +0300

    Added coupon validation for member registration

--- library_management/library_management/doctype/member/member.py
+++ library_management/library_management/doctype/member/member.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2024, Emanuel Fidelis and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Member(Document):
-	pass
+	def validate_coupon(member):
+		valid_coupons = ["WELCOME50", "LIBRARY2024", "READINGFUN"]
+		if member.coupon_code not in valid_coupons:
+			frappe.throw("Invalid Coupon Code")
+
